2e7206d5dc4e22563d267a55184a72b97b655c48
toyfather <leeseojun49@gmail.com>
2025-10-04 20:38:47 +0900

    useEffect import 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
